e7f4e506f948f4e760005dc100795bc2bb08357b
RodrigoAre12 <Rodriroroa@gmail.com>
2026-01-11 14:23:44 -0600

    Commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoAzulCerca.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public class AutoAzulCerca extends LinearOpMode {
+     @Override
+    public void runOpMode() {
+         Pose2d beginPose =  new Pose2d(-52, -52, Math.toRadians(225));
+         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+         // TRAYECTORIAS
+         Action preLoad = drive.actionBuilder(beginPose)
+                 .strafeToLinearHeading(new Vector2d(-38, -32), Math.toRadians(225))
+                 .build();
+         Pose2d shootingPose = new Pose2d(-38, -32, Math.toRadians(225));
+
+         Action stack1 = drive.actionBuilder(shootingPose)
+                 .splineToLinearHeading(new Pose2d(-8,  -28, Math.toRadians(266)), Math.toRadians(0))
+                 .build();
+         Pose2d fstack1 = new Pose2d(-8, -28, Math.toRadians(266));
+
+         Action take1 = drive.actionBuilder(fstack1)
+                 .strafeToLinearHeading(new Vector2d(-8, -54), Math.toRadians(266))
+                 .build();
+         Pose2d ftake1 = new Pose2d(-8, -54, Math.toRadians(266));
+
+         Action returnShootingPose = drive.actionBuilder(ftake1)
+                 .strafeToLinearHeading(new Vector2d(-38.1, -32), Math.toRadians(225))
+                 .build();
+         Pose2d fsecondshoot = new Pose2d(-38.1, -32, Math.toRadians(225));
+
+         Action stack2 = drive.actionBuilder(fsecondshoot)
+                 .splineToLinearHeading(new Pose2d(16,-28, Math.toRadians(266)), Math.toRadians(0))
+                 .build();
+         Pose2d fstack2 = new Pose2d(16, -28, Math.toRadians(266));
+
+         Action take2 = drive.actionBuilder(fstack2)
+                 .strafeToLinearHeading(new Vector2d(16, -50), Math.toRadians(266))
+                 .build();
+         Pose2d ftake2 = new Pose2d(16, -50, Math.toRadians(266));
+
+         Action returnFromStack2 = drive.actionBuilder(ftake2)
+                 .strafeToLinearHeading(new Vector2d(-38.2, -32), Math.toRadians(225))
+                 .build();
+
+
+         telemetry.addData("Estado", "Esperando inicio...");
+        telemetry.update();
+        waitForStart();
+
+         if (opModeIsActive()) {
+             Actions.runBlocking(
+                     new ParallelAction(
+                             actions.maintainShooter(), // PIDF de fondo
+
+                             new SequentialAction(
+                                     // --- PRELOAD ---
+                                     new ParallelAction(
+                                             preLoad,
+                                             actions.enableShooter(),
+                                             // Truco: Abrir puerta ANTES de llegar (Super rápido)
+                                             // Úsalo solo si las pelotas no se caen al moverse
+                                             actions.openGate()
+                                     ),
+                                     // Como ya abrimos puerta, el feed es casi instantáneo
+                                     actions.feed(1.5),
+
+                                     // --- IR AL STACK 1 (TODO PARALELO) ---
+                                     // Aquí eliminamos el "static collect". El robot se mueve,
+                                     // apaga shooter y prende intake AL MISMO TIEMPO.
+                                     new ParallelAction(
+                                             stack1,                 // Moverse
+                                             actions.collect(),      // Prender Intake
+                                             actions.stopShooter()   // Apagar Shooter
+                                     ),
+                                     take1, // Entrar al stack
+
+                                     // --- REGRESO 1 (PREPARANDO DISPARO) ---
+                                     new ParallelAction(
+                                             returnShootingPose,
+                                             actions.stopCollect(),
+                                             actions.enableShooter(),
+                                             actions.openGate() // Pre-abrir puerta en el camino de regreso
+                                     ),
+
+                                     // DISPARO CICLO 1 (Instantáneo)
+                                     actions.feed(1.5),
+
+                                     // --- IR AL STACK 2 (TODO PARALELO) ---
+                                     new ParallelAction(
+                                             stack2,
+                                             actions.collect(),
+                                             actions.stopShooter()
+                                     ),
+                                     take2,
+
+                                     // --- REGRESO 2 ---
+                                     new ParallelAction(
+                                             returnFromStack2,
+                                             actions.stopCollect(),
+                                             actions.enableShooter(),
+                                             actions.openGate() // Pre-abrir
+                                     ),
+
+                                     // DISPARO CICLO 2
+                                     actions.feed(1.5), // Un poco más largo para asegurar vaciado
+
+                                     // FIN
+                                     actions.stopShooter(),
+                                     actions.stopCollect()
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Commands.java
@@ -0,0 +1,217 @@
+package org.firstinspires.ftc.teamcode.;
+
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Feeder;
+import org.firstinspires.ftc.teamcode.Subsystems.MecanumDriveSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.Roller;
+import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
+import org.firstinspires.ftc.teamcode.Subsystems.Turret;
+import org.firstinspires.ftc.teamcode.Subsystems.Vision;
+
+
+
+public class Commands {
+    private final Hardware robot;
+
+    // PIDF
+    public static double TARGET_TPS = 1480;
+    public static double F = 0.000425;
+    public static double P = 0.007;
+    public static double I = 0;
+    public static double D = 0;
+
+    //Servo
+    public static double BLOCK_POS = 0.43;       // Cerrado
+    public static double LEFT_OPEN = 0.0;        // Izquierda Abierto
+    public static double RIGHT_OPEN = 0.945;     // Derecha Abierto
+
+    // DELAY entre que sube la patita y se activa el transfer
+    private static final double SERVO_PHYSICAL_DELAY = 0.45;
+
+    // Potencias de los motores
+    private static final double TRANSFER_POWER = 1;
+    private static final double INTAKE_POWER = 1;
+
+    // States
+    private boolean shooterEnabled = false;
+    private boolean isGateOpen = false; // Esto es para saber si está abierto
+
+    public RobotActions(Hardware hardware) {
+        this.robot = hardware;
+    }
+
+    // --- PIDF (BACKGROUND) ---
+    public class MaintainShooterSpeed implements Action {
+        private final ElapsedTime timer = new ElapsedTime();
+        private double lastError = 0;
+        private double integralSum = 0;
+        private boolean isFirstRun = true;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!shooterEnabled) {
+                robot.flyWheel.setPower(0);
+                return true;
+            }
+
+            double currentTPS = robot.flyWheel.getVelocity();
+            double error = TARGET_TPS - currentTPS;
+            double timerSeconds = timer.seconds();
+
+            if (isFirstRun) { timerSeconds = 0.0001; isFirstRun = false; }
+
+            double derivative = (error - lastError) / timerSeconds;
+            if (Math.abs(error) < 200) integralSum += (error * timerSeconds);
+            else integralSum = 0;
+
+            timer.reset();
+            lastError = error;
+
+            double feedforward = F * TARGET_TPS;
+            double pid = (P * error) + (I * integralSum) + (D * derivative);
+            robot.flyWheel.setPower(feedforward + pid);
+
+            packet.put("Shooter TPS", currentTPS);
+            return true;
+        }
+    }
+
+    public class EnableShooter implements Action {
+        @Override public boolean run(@NonNull TelemetryPacket packet) { shooterEnabled = true; return false; }
+    }
+    public class DisableShooter implements Action {
+        @Override public boolean run(@NonNull TelemetryPacket packet) { shooterEnabled = false; return false; }
+    }
+
+    // Esto se activa cuando me dirijo
+    public class OpenGate implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            robot.left.setPosition(LEFT_OPEN);
+            robot.right.setPosition(RIGHT_OPEN);
+            isGateOpen = true; // Marcamos que ya está abierta
+            return false;
+        }
+    }
+
+    // Feed Optimizado
+    public class FeedShooter implements Action {
+        private final ElapsedTime runTimer = new ElapsedTime();
+        private final ElapsedTime servoTimer = new ElapsedTime();
+
+        private boolean initialized = false;
+        private boolean servosCommandedOpen = false;
+        private final double timeToFeed;
+
+        public FeedShooter(double timeSeconds) {this.timeToFeed = timeSeconds;}
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double currentTPS = robot.flyWheel.getVelocity();
+            double error = TARGET_TPS - currentTPS;
+            boolean rpmReady = Math.abs(error) < TOLERANCE;
+
+            if (!initialized) {
+                runTimer.reset();
+                // Si OpenGate antes, ya estaba abierto.
+                // Si no, asumimos cerradas.
+                servosCommandedOpen = isGateOpen;
+                if(!isGateOpen) servoTimer.reset(); // Solo vamos a resetear timer si vamos a abrir ahorita
+                initialized = true;
+            }
+
+            packet.put("DEBUG TPS", currentTPS);
+
+            if (runTimer.seconds() < timeToFeed) {
+                if (shooterEnabled && rpmReady) {
+
+                    // ABREN SERVOS (Si no estaban abiertos)
+                    if (!servosCommandedOpen) {
+                        robot.left.setPosition(LEFT_OPEN);
+                        robot.right.setPosition(RIGHT_OPEN);
+                        isGateOpen = true;
+                        servoTimer.reset();
+                        servosCommandedOpen = true;
+                    }
+
+                    // Si ya estaba abierta desde antes (isGateOpen era true), el delay es 0.
+                    boolean timeCheck = servoTimer.seconds() > SERVO_PHYSICAL_DELAY;
+
+                    if (timeCheck) {
+                        robot.transfer.setPower(TRANSFER_POWER);
+                        robot.intake.setPower(INTAKE_POWER * 0.5);
+                        packet.put(">","FIREEEEEEEEEEEEEEEEEEEEEEEEEEE");
+                    } else {
+                        // Esperando que se abra físicamente
+                        robot.transfer.setPower(0);
+                        robot.intake.setPower(0);
+
+                        // Reseteamos el runTimer para que no cuente el tiempo de espera (esto está muy chido, dilo en entrevista)
+                        runTimer.reset();
+                    }
+
+                } else {
+                    closeGateInternal();
+                    runTimer.reset();
+                }
+                return true;
+            } else {
+                closeGateInternal();
+                return false;
+            }
+        }
+
+        private void closeGateInternal() {
+            robot.left.setPosition(BLOCK_POS);
+            robot.right.setPosition(BLOCK_POS);
+            robot.transfer.setPower(0);
+            robot.intake.setPower(0);
+            isGateOpen = false; // Cerrada
+            servosCommandedOpen = false;
+        }
+    }
+
+    public class CollectAction implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            robot.left.setPosition(BLOCK_POS);
+            robot.right.setPosition(BLOCK_POS);
+            isGateOpen = false; // Aseguramos que está cerrado
+
+            robot.intake.setPower(INTAKE_POWER*0.8);
+            robot.transfer.setPower(TRANSFER_POWER*0.8);
+            return false;
+        }
+    }
+
+    public class StopCollect implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            robot.intake.setPower(0);
+            robot.transfer.setPower(0);
+            return false;
+        }
+    }
+
+    public class OuttakeAction implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            robot.intake.setPower(-INTAKE_POWER);
+            robot.transfer.setPower(-TRANSFER_POWER);
+            return false;
+        }
+    }
+
+    // GETTERS
+    public Action maintainShooter() { return new MaintainShooterSpeed(); }
+    public Action enableShooter() { return new EnableShooter(); }
+    public Action stopShooter() { return new DisableShooter(); }
+    public Action collect() { return new CollectAction(); }
+    public Action stopCollect() { return new StopCollect(); }
+    public Action outtake() { return new OuttakeAction(); }
+    public Action feed(double seconds) { return new FeedShooter(seconds); }
+    public Action openGate() { return new OpenGate(); }
